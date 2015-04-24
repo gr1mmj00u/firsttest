ea0ad090f4d16bd22f9a5798166e3b2085609d87
Stanislav Kharitonov <q3introx@mail.ru>
2015-04-24 18:17:04 +0400

    Доделал пару функций и исправил limit()

--- index.php
+++ index.php
@@ -1,106 +1,48 @@
 <?php
+	require_once 'models.php';
 
-class MyActiveRecord{
+ 	$config = array(
+	"dbtype"=>"mysql",
+	"login"=>"root",
+	"dbpass"=>"",
+	"dbhost"=>"192.168.1.2:3306",
+	"dbname"=>"library"
+	);
 
-	//public $dbtype;
-	//public $login;
-	//public $dbpass;
-	public $dbhost;
-	public $dbname;
-   
-	private $config = array();
-	public $query = "";
-	public $pdo;
-	public $prepare_query;
+	$db = new MyActiveRecord($config);
+	$columns = array("id", "name");
+	$table = "stas";
+	$a = "name";
+	$low_priority = false;
+	$ignore = false;
 
-	function __construct($config){/*Соединение с БД*/
-		$this->config = $config;
-		print_r($this->config);
-		$this->pdo = new PDO("$config[dbtype]:host=$config[dbname];dbname=$config[dbname]", $config['login'], $config['dbpass']);
-		
-	}
+	$values_query = array(
+		"name"=>"djon",
+		"surname"=>"boriawdawdsov"
+		);
 
-	function select($columns){
-		if (gettype($columns)=="string" AND strlen($columns) < 100 AND preg_match("/^[A-Za-zА-Яа-я0-9ё\s,`.:_-]+$/u",$columns)) {//Проверка переменной 
-			//if (preg_match("#^[a-zA-Zа-яА-Я0-9\-_ `,]+$#",$str)) {
-				$this->query = "SELECT ".$columns;
-				return $this;
-		} 
-		else {
-			break;
-		}
-	}
+	$values_update = array(
+		"name45234"=>"stasik",
+		"surname"=>"pupok"
+		);
 
-	function from($table){
-		if (gettype($table)=="string" AND strlen($table) < 100 AND preg_match("/^[A-Za-zА-Яа-я0-9ё\s,`.:_-]+$/u",$table)) {
-			$this->query = $this->query." FROM $table";
-			return $this;
-		}
-		else {
-			break;
-		}
-	}
+try {
+	echo $db->select($columns)->from($table)->where($a,'=', 'stasik')->limit(4,4);//Пример Запроса SELECT
+	echo "<pre>";
+	print_r($db->execute()->fetchAll()); //извлечение результата запроса	
+	echo "<pre/>";
+//----------------------------------------------------------------------------------------------------
+	print_r($db->insert($table)->values($values_query));// Пример Запроса INSERT
+	$db->execute();
+//----------------------------------------------------------------------------------------------------
+	$db->delete()->from($table)->where($a, '>=', 15);//Пример запроса DELETE
+	$db->execute();
+//----------------------------------------------------------------------------------------------------
+	echo $db->update($table, $low_priority, $ignore)->set($values_update)->where($a, '=', 'Stasik');//Пример запроса UPDATE
+	$db->execute()->fetchAll();
 
-	function where($a, $b, $c){
-		if (gettype($a)=="string" AND strlen($a) < 100 AND preg_match("/^[=><!]+$/",$b) AND gettype($c)=="integer") {
-			$this->query = $this->query." WHERE $a $b :c";
-			$this->prepare_query['c'] = $c;
-			return $this;
-		} else {
-			break;
-		}
-	}
-
-	function limit($num){
-		if (gettype($num) == "integer")
-		{
-			$this->query = $this->query." LIMIT $num";
-			return $this;
-		}
-		else { 
-			break;
-		}
-	}
-
-	function insert(){
-
-	}
-
-	function update(){
-
-	}
-
-	function delete(){
-
-	}
-
-	function save(){
-		$result = $this->pdo->prepare($this->query);
-		$result->execute($this->prepare_query);
-		$data = $result->fetchAll(PDO::FETCH_ASSOC);
-		return $data;
-	}
-
-	function __destruct(){
-    	$this->pdo = null;
-    }
+} catch (Exception $e) {
+	 	echo $e->getMessage();
 }
-
-
-$config = array("dbtype"=>"mysql","login"=>"root","dbpass"=>"","dbhost"=>"192.168.1.2:3306","dbname"=>"library");
-
-$db = new MyActiveRecord($config);
-$columns = "`id`, `name`";
-$table = "stas";
-$a = "id";
-
-print_r($db->select($columns)->from($table)->where($a,'>=', 1)->limit(3));
-
-
-echo "<pre>";
-print_r($db->save());
-echo "<pre/>";
-
-echo $db->query;
 
 ?>
